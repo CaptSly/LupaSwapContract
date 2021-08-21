8675351baf94bedc1fb4aaa2906ca2fa013bd885
CaptSly <84409689+CaptSly@users.noreply.github.com>
2021-08-21 16:59:40 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
 contract Token {
-    string  public name = "GCASH TOKEN";
-    string  public symbol = "GCSH";
-    uint256 public totalSupply = 1000000000000000000000000000; // 1 TRILLION tokens
+    string  public name = "DApp Token";
+    string  public symbol = "DAPP";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8   public decimals = 18;
 
     event Transfer(
