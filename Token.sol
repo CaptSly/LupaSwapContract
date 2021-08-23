99ade59cb8e57d386137978d8cea227a379c17b6
CaptSly <84409689+CaptSly@users.noreply.github.com>
2021-08-23 21:17:12 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,83 +1,51 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
-// use latest solidity version at time of writing, need not worry about overflow and underflow
-
-/// @title ERC20 Contract 
 
 contract Token {
+    string  public name = "Lupa Token";
+    string  public symbol = "Lupa";
+    uint256 public totalSupply = 100000000000000000000000000000; // 1 Trillion tokens
+    uint8   public decimals = 18;
 
-    // My Variables
-    string public name;
-    string public symbol;
-    uint256 public decimals;
-    uint256 public totalSupply;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
-    // Keep track balances and allowances approved
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    // Events - fire events on state changes etc
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    constructor(string memory _name, string memory _symbol, uint _decimals, uint _totalSupply) public {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply; 
+    constructor() public {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    /// @notice transfer amount of tokens to an address
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    /// @return success as true, for transfer 
-    function transfer(address _to, uint256 _value) external returns (bool success) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
-        _transfer(msg.sender, _to, _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    /// @dev internal helper transfer function with required safety checks
-    /// @param _from, where funds coming the sender
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    // Internal function transfer can only be called by this contract
-    //  Emit Transfer Event event 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        // Ensure sending is to valid address! 0x0 address cane be used to burn() 
-        require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from] - (_value);
-        balanceOf[_to] = balanceOf[_to] + (_value);
-        emit Transfer(_from, _to, _value);
-    }
-
-    /// @notice Approve other to spend on your behalf eg an exchange 
-    /// @param _spender allowed to spend and a max amount allowed to spend
-    /// @param _value amount value of token to send
-    /// @return true, success once address approved
-    //  Emit the Approval event  
-    // Allow _spender to spend up to _value on your behalf
-    function approve(address _spender, uint256 _value) external returns (bool) {
-        require(_spender != address(0));
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    /// @notice transfer by approved person from original address of an amount within approved limit 
-    /// @param _from, address sending to and the amount to send
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    /// @dev internal helper transfer function with required safety checks
-    /// @return true, success once transfered from original account    
-    // Allow _spender to spend up to _value on your behalf
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
-        allowance[_from][msg.sender] = allowance[_from][msg.sender] - (_value);
-        _transfer(_from, _to, _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
         return true;
     }
-
 }
